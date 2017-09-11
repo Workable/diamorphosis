ccfec0915adad00331f3a4674f5964214e4ec315
Nikos Kostoulas <kostoulas@workable.com>
2017-09-11 18:12:54 +0300

    Revamp env2config module
    - Changes the signature of the module (see README)

---
+++ test/scenarios/replaceAll.js
@@ -0,0 +1,39 @@
+module.exports = {
+  description: 'should replace values from env, .env, test.json',
+  NODE_ENV: 'test',
+  input: [{ loadDotEnv: ['test'] }],
+  env: {
+    FROM_ENV_NUMBER: 2.0,
+    FROM_ENV_BOOLEAN: true,
+    FROM_ENV_STR: 'string',
+    FROM_ENV_ARRAY: ['g', 'h', 'i']
+  },
+  expected: {
+    nested: {
+      number: 1.3,
+      boolean: true,
+      str: 'string',
+      array: [1, 2, 3]
+    },
+    from: {
+      dot_env: {
+        number: 1.0,
+        boolean: true,
+        str: 'FROM_DOT_ENV_STR',
+        array: ['d', 'e', 'f']
+      }
+    },
+    from_env: {
+      number: 2.0,
+      boolean: true,
+      str: 'string',
+      array: ['g', 'h', 'i']
+    },
+    from_test_json: {
+      number: 1000.0,
+      boolean: false,
+      str: 'test',
+      array: ['a', 'b', 'c']
+    }
+  }
+};
