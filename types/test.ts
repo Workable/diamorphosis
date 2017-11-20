97fe9181439586b436556e5e6f2791c41eacc458
Nikos Kostoulas <kostoulas@workable.com>
2017-11-20 18:07:30 +0200

    Delete test.ts

--- types/test.ts
+++
@@ -1,7 +0,0 @@
-import diamorphosis from './diamorphosis';
-
-let isCorrectInput = { configFolder: 'bar' };
-let isNotCorrectInput = { foo: 'bar' };
-
-diamorphosis(isCorrectInput);
-diamorphosis(isNotCorrectInput);
