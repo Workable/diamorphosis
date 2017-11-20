5aa0398378f27c110f768a8bc5214c07f698ee11
Alkis Kalogeris <alkis@workable.com>
2017-11-20 18:08:48 +0200

    Add types for typescript projects (#4)
    * Add types for typescript projects
    
    * Delete test.ts

---
+++ types/diamorphosis.d.ts
@@ -0,0 +1,10 @@
+declare function diamorphosis(input: {
+  configFolder?: string;
+  configPath?: string;
+  envFolder?: string;
+  loadDotEnv?: string;
+}): void
+
+declare namespace diamorphosis {}
+
+export = diamorphosis;
